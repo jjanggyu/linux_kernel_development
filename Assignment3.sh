f779d03457308f685eda4a1d33f1fe77cb94398c
leechangyu <lcg6307@naver.com>
2020-10-01 05:44:51 +0900

    [MODIFY] simplified code

--- Assignment3.sh
+++ Assignment3.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-err() {
-    echo $1 1>&2
-    exit 1
-}
-
 ROWS=$1
 COLUMNS=$2
-
-if [[ ${ROWS} -lt 1 ]] || [[ ${COLUMNS} -lt 1 ]] ; then
-    err "The number of rows and columns must be positive numbers."
-fi
 
 for row in $(seq 1 ${ROWS}) ; do
     for col in $(seq 1 ${COLUMNS}) ; do
